89d7a7ff9480477dcf549a3fc3b290d1827273c3
danielPomo <danielnicolaspomo@gmail.com>
2023-03-09 08:59:57 -0300

    cambio en ej 17

--- ej17.js
+++ ej17.js
@@ -1,3 +1,10 @@
 //17- Realiza un script que muestre la posición de la primera vocal de un texto introducido por teclado.
 
 let text = prompt("Escribe algo")
+let arr= []
+for (let i = 0; i <= text.length; i++) {
+    if (text[i] ==="a" || text[i] ==="á" || text[i] ==="e" || text[i] ==="é" || text[i] ==="i" || text[i] ==="í" || text[i] ==="o" || text[i] ==="ó" || text[i] ==="u"|| text[i] ==="ú") {
+        arr.push(i)
+    }
+}
+document.write((Math.min(...arr)))
